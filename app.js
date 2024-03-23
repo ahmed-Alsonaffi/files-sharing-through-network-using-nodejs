88c958e309d86f44f967e3c47702357c2e1f9261
ahmed-Alsonaffi <ahmed.alsonaffi1@gmail.com>
2024-03-24 00:45:43 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express');
 
+const util = require('./util/helper');
+
+const ip = '192.168.1.6';
+const port = 4000;
+
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname,'views','main.html'));
-});
 
-app.listen(4000, () => {
-    console.log(`Server running at http://localhost:4000/`);
+app.get('/',util.getFiles);
+app.use(util.getFile);
+
+app.listen(port, ip, () => {
+    console.log(`Server running at http://${ip}:${port}/`);
   });
